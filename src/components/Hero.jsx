4bf31f284d261beb4b17d1e726d04294f01b4c0c
MartinMuru <mutwiri333@gmail.com>
2026-01-27 22:32:27 +0000

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div id='hero' className='flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white'>
         <div className='inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full'>
             <img className='w-20' src={assets.group_profile} alt=''/>
-            <p className='text-xs font-medium'>Inclusive communication, without barriers.</p>
+            <p className='text-xs font-medium'>Inclusive communication without barriers.</p>
         </div>
         <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>
             Everyone deserves to be <span className='bg-gradient-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>understood.</span>
