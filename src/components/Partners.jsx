1aba2970189fcd98d063c0ffce2ce351465a8b99
MartinMuru <mutwiri333@gmail.com>
2026-01-28 12:27:32 +0000

    Update Partners.jsx

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import { company_logos } from '../assets/assets'
 
-const Partners = () => {
+const Partners = ({ theme }) => {
   return (
-   <div className='flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80'>
-        <h3 className='font-semibold'>Our Partners</h3>
+    <div className="flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80">
+      <h3 className="font-semibold">Our Partners</h3>
 
-        <div className='flex items-center justify-center flex-wrap gap-10 m-4'>
-            {company_logos.map((logo, index)=> (
-                <img key={index} src={logo} alt='' className=' h-8 
-    sm:h-10 
-    md:h-12 
-    lg:h-16 
-    xl:h-20 
-    object-contain
-    dark:drop-shadow-xl'/>
-            ))}
-        </div>
+      <div className="flex items-center justify-center flex-wrap gap-10 m-4">
+        {company_logos.map((logo, index) => (
+          <img
+            key={index}
+            src={theme === 'dark' ? logo.dark : logo.light}
+            alt={logo.name}
+            className="h-8 sm:h-10 md:h-12 lg:h-16 xl:h-20 object-contain dark:drop-shadow-xl"
+          />
+        ))}
+      </div>
     </div>
   )
 }
